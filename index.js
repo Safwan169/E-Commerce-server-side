739fa3f725b639e471747845b3653046718e1a3e
safwan 169 <safwanhossainmridul@gmail.com>
2024-08-17 19:11:21 +0600

    done

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
-const port=process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 require('dotenv').config()
-const cors =require('cors')
+const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const json = require('body-parser/lib/types/json')
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.6zehkma.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,54 +26,72 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-const database=client.db("E-Commerce")
-const data_Product=database.collection("Product-data")
+    const database = client.db("E-Commerce")
+    const data_Product = database.collection("Product-data")
 
-    app.post('/product',async (req,res)=>{
-        const name=req.body.name;
-        const brand=req.body.brand;
-        const Category =req.body.category;
-        const price=req.body.price;
-        console.log(name)
+app.post('/products',async(req,res)=>{
+  const cursor = data_Product.find()
+  const result = await cursor.toArray()
+  return res.send(result)
 
-        if(name){
+})
 
-          const query = { title: `${name}` };
-          
-          const cursor=await data_Product.findOne(query)
+    app.post('/product', async (req, res) => {
+      const name = req.body.name;
+      const brand = req.body.brand;
+      const Category = req.body.category;
+      const price = req.body.price;
+      // console.log(brand)
 
-          // const result= await cursor.toArray()
-        return  res.send(cursor)
+      if (name) {
+
+        const query = { title: `${name}` };
+
+        const cursor = await data_Product.findOne(query)
+        if (cursor) {
+          return res.send([cursor])
+
+
+        }else{
+          return res.send([])
         }
+        // const result= await cursor.toArray()
+      }
 
-        else if (brand|| Category||price) {
+      else if (brand || Category || price) {
+      
 
-          
-          const query = { 
-             $or: [
-            { brand: brand },
-            { price: price },
-            { Category : Category  }
-          ] };
-          
-          const cursor= data_Product.find(query)
-          const result= await cursor.toArray()
-        return  res.send(result)
-        } 
-        
-        else{
-          const cursor=data_Product.find()
-          const result= await cursor.toArray()
-        return  res.send(result)
-        }
+
+       
+
+        const cursor = data_Product.find(query)
+        const result = await cursor.toArray()
+        // return res.send(json(result))
+      }
+
+      else {
+        const cursor = data_Product.find().sort({ price: -1 })
+        const result = await cursor.toArray()
+        // return res.send(result)
+      }
 
     })
- 
 
 
 
+app.post('/all',async (req,res)=>{
 
-  
+  const data=parseInt(req.query.size)
+  const cursor = data_Product.find()
+  .skip(data*parseInt(8))
+  .limit(parseInt(8))
+  const result = await cursor.toArray()
+  return res.send(result)
+
+
+})
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -86,9 +105,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('done')
-  })
+  res.send('done')
+})
 
 app.listen(port)
-  
-  
+
+
